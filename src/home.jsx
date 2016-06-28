e30a59b3f2aef8636084db168c13c1e6076031db
Amy Tang <amytang@infotech.com>
2016-06-28 08:02:16 -0400

    changed home.jsx file - adding import for header

--- src/home.jsx
+++ src/home.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HeaderComponent from './components/header';
+
+// Styles
 import 'style!css!./style.css';
 
 export default class Main extends React.Component {
 
   render() {
-    return  <header className="page-header">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-7">
-              <h1>Playing Here</h1>
-            </div>
-            <div className="col-md-5">
-
-            </div>
-          </div>
-        </div>
-      </header>
+    return  <HeaderComponent title='Playing here' />
   }
 }
 
@@ -24,3 +16,5 @@ ReactDOM.render(
   <Main />,
   document.getElementById('root')
 );
+
+
