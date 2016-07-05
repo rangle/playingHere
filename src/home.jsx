375c658f082e335542e11fa578bc6a5bbaf31c36
frachane <frachane@sheridancollege.ca>
2016-07-04 23:50:22 -0400

    Added footer component and changed class to className in header component

--- src/home.jsx
+++ src/home.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import HeaderComponent from './components/header.jsx';
 import CityListing from './components/city-list.jsx'
 import MapComponent from './components/map.jsx'
+import FooterComponent from './components/footer.jsx'
 
 const cityList = ['Paris', 'Toronto', 'New York', 'London (UK)', 'LA'];
 
@@ -18,6 +19,9 @@ export default class Main extends React.Component {
                       <MapComponent lat={43.652644} long={-79.381769} zoom={13} mapType={google.maps.MapTypeId.ROADMAP}/>
                     </div>
                   </div>
+                  <FooterComponent title='Playing Here' bodyTitle='A concert finding application in collaboration with:'
+                  body='Amy Tang, Chanelle Francis, Janelle Hinds and Tiffany Nogueira'
+                  MentorName='Abdella Ali' MentorImg='della.jpeg'/>
                 </section>
 
             </div>
