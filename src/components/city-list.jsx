b73448310fb2cd04c37a218fb315491ed55f0136
Abdella <abdellaalioncan@gmail.com>
2016-07-21 19:32:03 -0400

    example of how to combine redux with react and maps

--- src/components/city-list.jsx
+++ src/components/city-list.jsx
@@ -13,23 +13,35 @@ export default class CityListing extends React.Component {
 
   };
 
+  static propTypes = {
+    cities: React.PropTypes.array
+  };
+
   constructor(props){
     super(props);
     this.state = {
-      cityNames: props.cities
+      cities: props.cities
     }
   }
 
   filterCities = (event) =>
     this.setState({
-      cityNames: this.props.cities
-        .filter(city => city.toLowerCase()
+      cities: this.props.cities
+        .filter(cityObj => cityObj.label.toLowerCase()
         .includes(event.target.value.toLowerCase()))
     });
+
+
 
   render = () => <div className="col-md-4">
                   <h3>Top 5 Cities:</h3>
                   <input style={this.myStyle.searchform} type="text" placeholder="Search cities..." onChange={this.filterCities}/>
-                    {this.state.cityNames.map((city, i) => <h4 key={i} style={this.myStyle.rowStyle}>{city}</h4>)}
+                    {this.state.cities.map((city, i) =>
+                      <h4 key={i}
+                          onClick={() => this.props.setSelectedCity(city)}
+                          style={this.myStyle.rowStyle}>
+                        {city.label}
+                      </h4>
+                    )}
                  </div>
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import test from './test-reducer';
 import artist from './artist-reducer';
+import cities from './city';
 
 export {
   test,
-  artist
+  artist,
+  cities
 };
