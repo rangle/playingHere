1ca51b0ca95a4df6988e99435c41b41e24f2c8c7
Chanelle Francis <frachane@sheridancollege.ca>
2016-07-25 19:48:57 -0400

    Added autocomplete to map city search bar (#19)
    * added autocomplete to the city search bar
    
    * Added geolocation to find current location

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -7,6 +7,10 @@ myStyle = {
     width: '100%',
     height: '400px',
     margin:'0 auto 30px auto'
+  },
+  inputSize: {
+    width: '50%',
+    marginBottom:'10px'
   }
 };
 
@@ -23,14 +27,25 @@ let mapProp = {
 // this.ref is referencing the DOM element(used instead of getElement)
 this.mapObj = new google.maps.Map(this.refs.myMap, mapProp);
 
-let submit = document.getElementById('submit');
-let geocoder = new google.maps.Geocoder();
+const submit = document.getElementById('submit');
+const geolocation = document.getElementById('geolocation');
+let city = document.getElementById('cityInput');
+this.geocoder = new google.maps.Geocoder();
+const autocomplete = new google.maps.places.Autocomplete(city);
+        autocomplete.bindTo('bounds', this.mapObj);
 
-submit.addEventListener('click', ()=>{
-this.geocodeAddress(geocoder, this.mapObj);
+
+geolocation.addEventListener('click', () => {
+  navigator.geolocation.getCurrentPosition((position) => {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        this.mapObj.setCenter(new google.maps.LatLng(pos.lat, pos.lng), this.props.zoom);
 });
 
 
+});
 };
 
 
@@ -67,11 +82,17 @@ let address = document.getElementById('cityInput').value;
   render() {
     return (
     <div className="col-md-8">
-      <input id="cityInput" type="text" className="form-control input-lg" />
-      <input id="submit" type="submit" className="btn btn-default btn-lg"/>
+    <div class="input-group">
+      <input type="text" class="form-control"id="cityInput"  placeholder="City Name" style={this.myStyle.inputSize} />
+      <span class="input-group-btn">
+        <button class="btn btn-secondary" id="submit" onClick= {() => this.geocodeAddress(this.geocoder, this.mapObj)} type="button">Submit</button>
+        <button class="btn btn-secondary" id="geolocation" type="button">Current Location</button>
+      </span>
+    </div>
       <div id="googleMap" ref="myMap" style={this.myStyle.mapSize}>
       </div>
     </div>
+
     )
   }
 }
